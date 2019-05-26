40a1b9a759643cef4fa2fcbac500d68ee1626a0c
Ray <cutejerry@foxmail.com>
2019-05-26 10:46:46 -0700

    support export to csv in Admin

--- SignModel/admin.py
+++ SignModel/admin.py
@@ -2,12 +2,44 @@ from django.contrib import admin
 from SignModel.models import Course
 from SignModel.models import Record
 
+import csv
+from django.http import HttpResponse
+
 class CourseAdmin(admin.ModelAdmin):
     pass
 
 class RecordAdmin(admin.ModelAdmin):
     list_display = ('name','phone_num','created_time')
     search_fields = ('name','phone_num','course_list__name',)
+    actions = ["export_as_csv"]
+    list_per_page = 500
+
+    def export_as_csv(self, request, queryset):
+        opts = self.model._meta
+        field_names = [f.name for f in opts.get_fields()]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment;filename={}.csv'.format(opts.verbose_name.encode('utf-8'))
+        writer = csv.writer(response)
+        writer.writerow(field_names)
+
+        for obj in queryset:
+            row = []
+            for field in field_names:
+                if(field == 'course_list'):
+                    info = ""
+                    for s in getattr(obj, "course_list").all():
+                        info = info + s.name + ','
+                    info = info[:-1]
+                # if(type(info).__name__ == 'str'):
+                #     info = info.encode('gb2312')
+                else:
+                    info = getattr(obj, field)
+                row.append(info)
+            writer.writerow(row)
+
+        return response
+    export_as_csv.short_description = "µ¼³öµ½Excel"
 
 # Register your models here.
 admin.site.register(Course, CourseAdmin)
--- SignModel/models.py
+++ SignModel/models.py
@@ -10,7 +10,7 @@ class Course(models.Model):
 # Create your models here.
 class Record(models.Model):
     name = models.CharField(max_length=64, verbose_name="已登记记录")
-    grade = models.CharField(max_length = 1, default = "0")
+    grade = models.CharField(max_length =64, default = "未选择")
     course_list = models.ManyToManyField("Course")
     phone_num = models.CharField(max_length=13, unique=True, verbose_name="电话号码")
     email = models.EmailField(null = True, blank = True)
